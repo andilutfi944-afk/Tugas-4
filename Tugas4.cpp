2baee131b42256602210a55cabf21af67d9cfdb6
andilutfi944-afk <andilutfi944@gmail.com>
2026-03-09 15:47:15 +0700

    Menambahkan int main

--- Tugas4.cpp
+++ Tugas4.cpp
@@ -3,15 +3,4 @@ using namespace std;
 
 int main(){
     int x;
-
-    srand(time(0));
-    x = rand() % 10;
-
-    if (x==5){
-        x = x+1;
-        cout<<"nilai x sudah dikoreksi menajdi : "<<x<<endl;
-
-    }
-
-    cout << "Bilangan yang diberikan = "<<x;
 }
