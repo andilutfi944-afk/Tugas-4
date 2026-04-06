30be978c131c2c90b3ac62b156464ad1c41129a5
andilutfi944-afk <andilutfi944@gmail.com>
2026-04-06 15:56:58 +0700

    Menambahkan trusck orang dan int main juga laman akhir menapilkan.

--- arraystruckm3.cpp
+++ arraystruckm3.cpp
@@ -1,6 +1,40 @@
 #include <iostream>
 using namespace std;
 
-struct detailalamat{
-
+struct detailalamat
+{
+    string kota;
+    string desa;
 };
+
+struct orang
+{
+    string nama;
+    detailalamat alamat;
+    int umur;
+};
+
+int main()
+{
+    orang mhs;
+
+    cout << "nama: ";
+    cin >> mhs.nama;
+
+    cout << "alamat kota: ";
+    cin >> mhs.alamat.kota;
+
+    cout << "alamat desa: ";
+    cin >> mhs.alamat.desa;
+
+    cout << "umur: ";
+    cin >> mhs.umur;
+
+    cout << endl;
+
+    // menampilkan
+    cout << "nama: " << mhs.nama << endl;
+    cout << "kota: " << mhs.alamat.kota << endl;
+    cout << "desa: " << mhs.alamat.desa << endl;
+    cout << "umur: " << mhs.umur << endl;
+}
