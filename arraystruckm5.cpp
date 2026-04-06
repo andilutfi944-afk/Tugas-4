bac6e7e2d20fca95dfd834142b9948f16fc7798c
andilutfi944-afk <andilutfi944@gmail.com>
2026-04-06 16:43:35 +0700

    Menambahkan struct detailAlamat, truct Orang dan int main.

--- arraystruckm5.cpp
+++ arraystruckm5.cpp
@@ -1,2 +1,19 @@
 #include<iostream>
 using namespace std;
+
+struct detailAlamat
+{
+    string desa;
+    string kota;
+};
+
+struct Orang
+{
+    string nama;
+    detailAlamat alamat;
+    int umur;
+};
+
+int main(){
+
+}
