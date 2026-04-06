3e96bd7585ca8a20afdf2728d3d814fe2ab26360
andilutfi944-afk <andilutfi944@gmail.com>
2026-04-06 16:44:46 +0700

    Menambahkan for (int i = 0; i <= 2; i++)

--- arraystruckm2.cpp
+++ arraystruckm2.cpp
@@ -11,18 +11,13 @@ struct orang
 int main()
 {
     orang mhs;
-    
     cout << "nama: ";
     cin >> mhs.nama;
-    
     cout << "alamat: ";
     cin >> mhs.alamat;
-    
     cout << "umur: ";
     cin >> mhs.umur;
-    
     cout << endl;
-    
     // menampilkan
     cout << "nama: " << mhs.nama << endl;
     cout << "alamat: " << mhs.alamat << endl;
